dec29cc756d9732a2f644f72965d1b9caf8964b7
iTimeTraveler <xuewenlong.2008@gmail.com>
2016-10-09 10:46:13 +0800

    Site updated: 2016-10-09 10:46:09

--- js/home.js
+++ js/home.js
@@ -1,4 +1,19 @@
-function insert(){
-   var insertText = "<table><tr><td>any thing</td></tr></table>";
-   document.getElementById("insert").innerHTML(insertText);
+var navbar = document.getElementsByClassName("intro-navigate")[0],
+	h = window.innerHeight;
+
+window.addEventListener("scroll", scrollHandler);
+
+function getStyle(obj, attri) {
+ 	return obj.currentStyle ? obj.currentStyle[attri] : window.getComputedStyle(obj, null)[attri];
+}
+
+function scrollHandler(e) {
+	 var event = e || window.event,
+	     target = event.target || event.srcElement;
+	 var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	 if (scrollTop > h - navbar.offsetHeight) {
+	     navbar.classList.add("fixed");
+	 } else {
+	     navbar.classList.remove("fixed");
+	 }
 }
